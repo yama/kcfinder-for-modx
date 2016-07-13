f0d1d78dcb5c45d5275597c6a1b98d7d56ac7f76
yamamoto <yamamoto@kyms.jp>
2016-07-13 21:47:52 +0900

    デザインをシンプルに調整

--- plugins/kcfinder/kcfinder/cache/theme_default.js
+++ plugins/kcfinder/kcfinder/cache/theme_default.js
@@ -1,1 +1,9 @@
-$.each(["loading.gif","ui-icons_black.png","ui-icons_blue.png","ui-icons_white.png"],function(b,a){new Image().src="themes/default/img/"+a});
+// Preload some images
+$.each([
+    "loading.gif",
+    "ui-icons_black.png",
+    "ui-icons_blue.png",
+    "ui-icons_white.png"
+], function(i, img) {
+    new Image().src = "themes/default/img/" + img;
+});
